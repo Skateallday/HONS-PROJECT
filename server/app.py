778a76d1bd4c2dbbf9e373316c2816f86dfabba2
marc Inglis <marcinglis91@hotmail.com>
2020-01-10 14:57:22 +0000

    Server/Static Folder Created, React Installed

---
+++ server/app.py
@@ -0,0 +1,59 @@
+import random
+import os
+import sqlite3
+from flask import Flask, render_template, request, session, g, redirect, flash, url_for
+from flask_bcrypt import Bcrypt, generate_password_hash, check_password_hash
+
+from flask_wtf.csrf import CSRFProtect, CSRFError
+
+
+
+app = Flask(__name__, static_folder='../static', template_folder='../static/frontend/templates')
+bcrypt = Bcrypt(app)
+csrf = CSRFProtect(app)
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+@app.before_request
+def before_request():
+        g.username = None
+        if 'username' in session:
+                g.username = session['username']
+
+@app.route('/')
+def index():
+        return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    registerForm = registration(request.form)   
+
+    if request.method == 'POST':
+            pw_hash =bcrypt.generate_password_hash(registerForm.password.data)
+            newEntry = [((registerForm.username.data), pw_hash, (registerForm.emailAddress.data))]
+            conn =sqlite3.connect('testData.db')
+            print ("Opened database successfully")
+            with conn:
+                    c =conn.cursor()
+                    try:
+                            sql = '''INSERT INTO users (username, password, email) VALUES(?,?,?)'''
+                            c.executemany(sql, newEntry)
+                            print ("Insert correctly")
+                    except:
+                            flash("This is already an account, please log in with those details or change details.")
+                            return render_template("register.html", form=registerForm)
+                            c.commit()
+
+                    flash((registerForm.username.data) + " Successfully Registered!")
+                    return redirect('login')
+            return render_template("register.html", form=registerForm)
+
+    return render_template('register.html', form=registerForm)
+
+
+if __name__ == '__main__':
+      app.run('localhost', 5000, debug=True)
